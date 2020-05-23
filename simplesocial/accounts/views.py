203b666ee4af162c68babb97d8385444cc73ca63
Tchea JiaWei <tcheajiawei@Hotmail.com>
2020-05-23 21:38:13 +0800

    fix redirect after signup bug

--- simplesocial/accounts/views.py
+++ simplesocial/accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.contrib.auth import login,logout
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
@@ -8,5 +8,5 @@ class SignUp(CreateView):
     form_class = forms.UserCreateForm
     #Once user has successfully submit their sign up form
     #redirect user to login page
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy("accounts:login")
     template_name = 'accounts/signup.html'
