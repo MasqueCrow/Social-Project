44f4ab6278c0101d5f4300760a2a944b43e81e35
Tchea JiaWei <tcheajiawei@Hotmail.com>
2020-05-23 18:03:53 +0800

    Create User Model and forms

--- simplesocial/accounts/views.py
+++ simplesocial/accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import CreateView
 
+from accounts import forms
 # Create your views here.
+class SignUp(CreateView):
+    form_class = forms.UserCreationForm
+    #Once user has successfully submit their sign up form
+    #redirect user to login page
+    success_url = reverse_lazy('login')
+    template_name = 'accounts/signup.html'
