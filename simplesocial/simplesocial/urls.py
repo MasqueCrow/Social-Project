9eebf1773e6888ae340cf1bc3c4dc4e0270922d5
Tchea JiaWei <tcheajiawei@Hotmail.com>
2020-05-23 19:32:05 +0800

    Revert "add test and thanks pages"
    This reverts commit 9e58cb9e5593ec51a1caa8b6177474b97402270f.

--- simplesocial/simplesocial/urls.py
+++ simplesocial/simplesocial/urls.py
@@ -15,13 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from simplesocial import views
+from simplesocial.views import HomePage
 
 urlpatterns = [
-    path('',views.HomePage.as_view(),name='home'),
+    path('',HomePage.as_view(),name='home'),
     path('admin/', admin.site.urls),
     path('accounts/',include('accounts.urls',namespace='accounts')),
     path('accounts/',include('django.contrib.auth.urls')),
-    path('test/',views.TestPage.as_view(),name='test'),
-    path('thanks/',views.ThanksPage.as_view(),name='thanks')
 ]
